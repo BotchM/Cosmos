d4af2943a9d8da4de99efb418771f4a1e4040c5f
Botch <botchm@hotmail.com>
2017-12-21 04:36:30 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var cosmic = require('./cosmos')
+
+//cosmic.pinit()
+hash = cosmic.addFile('public/MindFieldS2E1.mp4')
+// cosmic.pinit()
+// cosmic.showPins()
---
+++ cosmos/src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Dropzone from 'react-dropzone'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = { files: [] }
+  }
+
+  onDrop(files) {
+    this.setState({
+      files
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Cosmos</h1>
+        </header>
+        <p className="App-intro">
+
+        </p>
+        <section>
+          <div className="dropzone">
+            <Dropzone onDrop={this.onDrop.bind(this)}>
+              <p>Try dropping some files here, or click to select files to upload.</p>
+            </Dropzone>
+          </div>
+          <aside>
+            <h2>Dropped files</h2>
+            <ul>
+              {
+                this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+              }
+            </ul>
+          </aside>
+        </section>
+      </div>
+    );
+  }
+
+}
+
+export default App;
