2f5f170804f0f4637a69e4ac6681d7380f006555
Mahmoud <mahmoud.shehata@weltel.org>
2019-06-11 14:34:37 -0700

    polling for adding and deleting nodes mutliaddres in bluezelle

--- cosmos.js
+++ cosmos.js
@@ -51,15 +51,23 @@ var cosmos = {
      * Delegate one node randomly to restart
      */
     poller.onPoll(async () => {
-      let keys = (await blue.getKeys()).length
+      let keys = await blue.getKeys()
 
       // check all nodes connect if not delete the one that isnt connecting
-      if(keys > k){
-        k = keys;
+      if(keys.length > k){
+        k = keys.length;
         console.log('New node joined!')
-      }else{
-        console.log('Total nodes: ', keys)
+        console.log('Total nodes: ', keys.length)
+      }else if (keys.length < k){
+        console.log('Total nodes: ', keys.length)
+        k = keys.length
         // check all connections and delete diconnected nodes
+        for(let key of keys = await blue.getKeys()){
+          if(key !== ipv4){ 
+            value = await blue.read(key)
+            await cosmos.swarmConnect(value, key)
+          }
+        }
       }
       poller.poll(); 
     });
@@ -132,13 +140,13 @@ var cosmos = {
   },
   swarmConnect: async(addr, key) => {
     try {
-      await ipfs.swarm.connect(addr).then(obj => console.log(key, obj.Strings))
+      await ipfs.swarm.connect(addr)//.then(obj => console.log(key, obj.Strings))
     } catch (e) {
       if (e.statusCode === 500 && (await blue.getKeys()).length > 2) {
         blue.deleteField(key)
         console.log(`${key} deleted`)
       }
-      console.log(e.statusCode)
+      console.log(e.statusCode, '\n')
     }
   }
 }
